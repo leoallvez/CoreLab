380f43a5fe8dc5044ee07b4744ff528d43548f52
leoallvez <leoallvez@hotmail.com>
2018-05-22 23:55:38 -0300

    Classe de Curso

---
+++ Models/Cadastros/Curso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Cadastros
+{
+    class Curso
+    {
+        public long? CursoID { get; set; }
+        public string Nome { get; set; }
+        public long? DepartamentoID { get; set; }
+        public Departamento departamento { get; set; }
+
+        //public virtual ICollection<CursoDiciplina> CursoDiciplinas { get; set; }
+    }
+}
